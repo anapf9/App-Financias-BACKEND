8b8d9836fbe5b5d402aa07bd3175e97776135dbd
Unknown <anapf9@gmail.com>
2019-09-07 16:19:37 -0300

    nodemon com docker

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const { prisma } = require("./generated/prisma-client"); // capturando o prisma 
 // No Graphql trabalha-se com tipo, onde os tipos tem seus "campos", e campos possuem "resolvers"
 // resolver são funções que implementam uma lógica para devolver valores para os campos
 const resolvers = {
-  // mapeia a estrutura que esta no shema.graphql
-  Query: {
-    user(parent, args, context, info) {
-      return prisma.user({ id: args.id });
-    }
-  }
+	// mapeia a estrutura que esta no shema.graphql
+	Query: {
+		user(parent, args, context, info) {
+			return prisma.user({ id: args.id });
+		}
+	}
 };
 
 /* const server = new GraphQLServer({
@@ -19,13 +19,13 @@ const resolvers = {
 }); */
 
 const server = new GraphQLServer({
-  typeDefs: `${__dirname}/schema.graphql`,
-  resolvers
+	typeDefs: `${__dirname}/schema.graphql`,
+	resolvers
 });
 
 server
-  .start()
-  .then(() => console.log("Server running on http://localhost:4000..."));
+	.start()
+	.then(() => console.log("Server running on http://localhost:4000..."));
 
 /* async function main() {
   await prisma.createUser({
