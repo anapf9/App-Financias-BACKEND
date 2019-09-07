13ee5ea6d250b16aae871d341e1f171406a1ab43
Unknown <anapf9@gmail.com>
2019-09-07 18:38:19 -0300

    mutations: singup e login com JWT e bcrypt

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+async function login(_, { email, password }, ctx, info) {
+	// via destructure extraimos os argumentos desejados
+	// abaixo tentamos buscar um usuario pelo email que estamos recebendo
+	const user = await ctx.db.query.user({ where: { email } });
+	if (!user) {
+		throw new Error("Invalid credential!");
+	}
+
+	const valid = await bcrypt.compare(password, user.password);
+	if (!valid) {
+		throw new Error("Invalid credential!");
+	}
+
+	const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "2h" });
+
+	return {
+		token,
+		user
+	};
+}
+
+async function singup(_, args, ctx, info) {
+	const password = await bcrypt.hash(args.password, 10);
+	const user = await ctx.db.mutation.createUser({
+		data: { ...args, password }
+	});
+
+	const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "2h" });
+	return {
+		token,
+		user
+	};
+}
+
+module.exports = {
+	login,
+	singup
+};
